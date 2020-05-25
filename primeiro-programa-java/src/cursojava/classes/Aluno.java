e4a8c89a1657d2a69e10a2f40c6ebf515aa0ec29
Raphael Veríssimo <62971242+raphaelverissimosb@users.noreply.github.com>
2020-05-25 15:09:31 -0300

    getters and setters

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -2,17 +2,18 @@ package cursojava.classes;
 
 public class Aluno {
 	
-	public String nome;
-	public int idade;
-	public String dataNascimento;
-	public String registroGeral;
-	public String numeroCpf;
-	public String nomaMae;
-	public String nomePai;
-	public String dataMatricula;
-	public String nomeEscola;
-	public String serieMatriculado;
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomaMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
 	
+	//constructors
 	public Aluno() {
 		
 	}
@@ -23,4 +24,70 @@ public class Aluno {
 		this.nome = nomePadrao;
 		this.idade = idadePadrao;
 	}
+	
+	//Getters and Setters
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+	public String getNomaMae() {
+		return nomaMae;
+	}
+	public void setNomaMae(String nomaMae) {
+		this.nomaMae = nomaMae;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	
+	
 }
