2d4c077e4fe5921f1356a67216f2dbcf71dbc80b
Raphael Veríssimo <62971242+raphaelverissimosb@users.noreply.github.com>
2020-08-07 17:19:10 -0300

    methods wuth parameters and arguments

--- JavaOnline/src/br/com/devdojo/javacore/introclasses/methods/Calculator.java
+++ JavaOnline/src/br/com/devdojo/javacore/introclasses/methods/Calculator.java
@@ -1,11 +1,20 @@
 package br.com.devdojo.javacore.introclasses.methods;
 
 public class Calculator {
-	
+	// methods without parameters 
 	public void sumTwoNumbers() {
 		System.out.println(5+5);
 	}
 	public void subTwoNumbers() {
 		System.out.println(5-5);
 	}
+	
+	public void multTwoNumbers(int number1 , int number2) {
+		 System.out.println(number1  * number2);
+	}
+	
+	public double divTwoNumbers(double num1 , double num2) {
+		return num1 / num2;
+	}
+	
 }
--- JavaOnline/src/br/com/devdojo/javacore/introclasses/methodstest/CalculatorTest.java
+++ JavaOnline/src/br/com/devdojo/javacore/introclasses/methodstest/CalculatorTest.java
@@ -10,7 +10,15 @@ public class CalculatorTest {
 		Calculator calc = new Calculator();
 		
 		calc.sumTwoNumbers();
+		calc.subTwoNumbers();
 		
+		// With parameters
+		calc.multTwoNumbers(5, 5);
+		// with return 
+		System.out.println(calc.divTwoNumbers(20, 2));
+		
+		double divNumberEx = calc.divTwoNumbers(50, 5);
+		System.out.println(divNumberEx);
 	}
 
 }
