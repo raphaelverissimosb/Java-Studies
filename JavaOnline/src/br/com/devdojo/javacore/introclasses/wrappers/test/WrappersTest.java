a465b50b27d7bb9bdbd867c80fd0e77fcbc298fb
Raphael Veríssimo <62971242+raphaelverissimosb@users.noreply.github.com>
2020-09-21 17:16:02 -0300

    wrappers part I

---
+++ JavaOnline/src/br/com/devdojo/javacore/introclasses/wrappers/test/WrappersTest.java
@@ -0,0 +1,15 @@
+package br.com.devdojo.javacore.introclasses.wrappers.test;
+
+public class WrappersTest {
+
+	Byte byteWrapper = 1;
+	Short shortWrapper = 1;
+	Integer intergerWrapper = 1;
+	Long longWrapper = 10L;
+	Float floatWrapper = 10F;
+	Double doubleWrapper = 10D;
+	Character characterWrappper = 'A';
+	Boolean booleanWrapper = true;
+	
+	
+}
